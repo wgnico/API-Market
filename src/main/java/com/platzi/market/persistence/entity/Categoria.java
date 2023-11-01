a3e75e4815fe46638828b21c62bf2fd79a7ddb9f
wgnico <nicoburgos1989@gmail.com>
2023-11-01 07:04:57 -0300

    p

---
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -0,0 +1,56 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+
+public class Categoria {
+    @Column(name = "id_categoria")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCategoria;
+
+    private String descripcion;
+
+    private boolean estado;
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+}
---
+++ src/main/java/com/platzi/market/web/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package com.platzi.market.web.config;
+
+
+
+public class SwaggerConfig {
+
+
+}
